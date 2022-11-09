9dedc7e8bffb4ee300b87d91b00299b2ec9f2e2c
Amanda Luiza <89023662+amandaluizay@users.noreply.github.com>
2022-11-09 15:35:06 -0300

    validations

---
+++ Leilão/src/Leilão.Domain/Models/Validations/PessoaJuridicaValidation.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leilão.Domain.Models.Validations
+{
+    public class PessoaJuridicaValidation : AbstractValidator<PessoaJuridica>
+    {
+        public PessoaJuridicaValidation()
+        {
+            RuleFor(c => c.CNPJ)
+                .NotEmpty().WithMessage("O campo {PropertyName} precisa ser fornecido");
+        }
+    }
+}
